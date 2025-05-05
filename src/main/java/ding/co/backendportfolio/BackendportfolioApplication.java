2c612cc8667f6cd8e2e256a0f90cb5860e244153
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-05-05 21:31:22 +0900

    feat: 콘솔 로그 추가

--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -10,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BackendportfolioApplication {
     public static void main(String[] args) {
+        System.out.println("취업 성공하자!!! 할 수 있다!!!");
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
 
